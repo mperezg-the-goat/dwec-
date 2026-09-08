1ee32e49312ef2bd8891ded281827d19fb5d40ba
Markel Pérez García (2DW3D) <m.perez.g@H015110.net>
2026-09-08 14:22:55 +0200

    2 Ariketa

---
+++ 2 Ariketa/script.js
@@ -0,0 +1,36 @@
+Zer desberdintasuna dago var eta let artean?
+
+-var: Funtzio-eremua du, eta berriz deklaratzeko aukera ematen du. let: forma modernoa, bloke-eremuarekin
+
+Eta let eta const artean?
+
+-let: Aldatzen diren aldagaietarako gomendatzen den aukera da. Eta const: aldagaiak ezin dira aldatu.
+
+
+Zer diferentzia dago == eta === artean?
+
+-==: Zerbait berdina bada adierazten du. Eta ===: Berdina da baina aldagai mota konparatzen du.
+
+Zer desberdintasuna dago while eta do-while artean?
+
+While:Kode osoa egiten du baina ez  baldintza ez bada zuzena while amaitzen da. Eta do- while berdina egiten du baina naiz eta baldintza zuzena ez izan, do dagoen kodea aldiz behin egiten da.
+
+Ordeztu if hau operatzaile ternario batekin:
+
+-
+
+let puntos = 75;
+let rol = (puntos >= 100) ? "VIP" : "Member";
+console.log(rol);
+ 
+Ordeztu kate hau letra-sarrera literalekin:
+ 
+ 
+let nombre = "Juan";
+let puntos = 85;
+let rol = "Member";
+console.log('Usuario: ${nombre}  | Puntos: ${puntos} | Rol: + ,${rol}');
+ 
+Zergatik aukeratu behar da txantiloi literalak "+" erabili baino?
+
+-Erosoa delako eta kodea txukun uzten duelako.
